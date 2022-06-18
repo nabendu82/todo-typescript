2520369350baa4a209ff6f9092b207d78dfc5fdd
nabendu82 <nabendu.biswas@gmail.com>
2022-06-18 18:12:17 +0530

    Final commit

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,42 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from "uuid"
 
-import confetti from 'canvas-confetti';
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.querySelector<HTMLFormElement>("#task-form")
+const input = document.querySelector<HTMLInputElement>("#task-title")
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+interface Task {
+    id: string
+    title: string
+    completed: boolean
+    createdAt: Date
+}
+
+form?.addEventListener("submit", e => {
+    e.preventDefault()
+    if (input?.value == "" || input?.value == null) return
+
+    const newTask: Task = {
+        id: uuidV4(),
+        title: input.value,
+        completed: false,
+        createdAt: new Date()
+    }
+
+    addItemToList(newTask)
+    input.value = ""
+})
+
+const addItemToList = (task: Task): void => {
+    const item = document.createElement("li")
+    const label = document.createElement("label")
+    const checkbox = document.createElement("input")
+    checkbox.addEventListener("change", () => {
+        task.completed = checkbox.checked
+        if(checkbox.checked) label.style.textDecoration = "line-through"
+    })
+    checkbox.type = "checkbox"
+    checkbox.checked = task.completed
+    label.append(checkbox, task.title)
+    item.append(label)
+    list?.append(item)
+}
